37d091e582660fdc23b2fc7ad1454b9f9fbcba28
velosomarcus <44785425+velosomarcus@users.noreply.github.com>
2024-12-14 14:14:15 -0800

    recording and sending movements back to robot

--- record_movements.py
+++ record_movements.py
@@ -1,4 +1,8 @@
+import json
 import time
+from collections import defaultdict
+from os import POSIX_SPAWN_OPEN
+
 from openlch.hal import HAL
 
 # Default IP is 192.168.42.1, which is the IP of the USB interface
@@ -7,31 +11,35 @@ robot = HAL("192.168.42.1")
 
 # positions = robot.servo.get_positions()
 input("hit Enter when ready to start moving")
-robot.servo.set_position(16, 0)
-
-movement_s16 = []
-positions = robot.servo.get_positions()
+robot.servo.disable_movement()
+robot.servo.set_position(16, 45)
+# robot.servo.disable_movement()
+movements = []
+# positions = robot.servo.get_positions()
+robot.servo.set_torque_enable([(i, False) for i in range(1, 17)])
 input("hit Enter when ready to start recording the movements")
 
 start_time = time.time()
-while time.time() - start_time < 2:
+while time.time() - start_time < 10:
     # Get current positions of all servos
     positions = robot.servo.get_positions()
     # ID, position, velocity [(1, 0, 0), (2, -50, 0), (3, 5, 20)...]
-    for item in positions:
-        if item[0] == 16:
-            movement_s16.append(item)
+    # for item in positions:
+    #     movements[item[0]].append(item)
 
-    time.sleep(0.1)
+    movements.append(positions)
+    time.sleep(0.05)
 
-print(movement_s16)
-
-print(len(movement_s16), "positions saved")
-    # print(type(positions))
+with open('movements.json', 'w') as o:
+    o.write(json.dumps(movements, indent=2))
 
 input("hit Enter to start moving")
+with open('movements.json', 'r') as r:
+    movements = json.loads(r.read())
 
-for item in movement_s16:
-    robot.servo.set_position(item[0], item[1])
-    time.sleep(0.1)
+for key in movements:
+    for servo in key:
+        robot.servo.set_position(servo[0], servo[1]) #, .05)
+    time.sleep(0.01)
 
+robot.servo.set_torque_enable([(i, False) for i in range(1, 17)])
