18facf665087527517f0c2dc4e59068f7d46601a
velosomarcus <44785425+velosomarcus@users.noreply.github.com>
2024-12-14 13:29:12 -0800

    testing

--- record_movements.py
+++ record_movements.py
@@ -6,19 +6,32 @@ robot = HAL("192.168.42.1")
 
 
 # positions = robot.servo.get_positions()
+input("hit Enter when ready to start moving")
+robot.servo.set_position(16, 0)
+
+movement_s16 = []
+positions = robot.servo.get_positions()
+input("hit Enter when ready to start recording the movements")
 
 start_time = time.time()
-# while time.time() - start_time < 5:
-#     # Get current positions of all servos
-positions = robot.servo.get_positions()
-    ## ID, position, velocity [(1, 0, 0), (2, -50, 0), (3, 5, 20)...]
-print(positions)
-print(type(positions))
+while time.time() - start_time < 2:
+    # Get current positions of all servos
+    positions = robot.servo.get_positions()
+    # ID, position, velocity [(1, 0, 0), (2, -50, 0), (3, 5, 20)...]
+    for item in positions:
+        if item[0] == 16:
+            movement_s16.append(item)
 
-print(type(positions[0]))
-# time.sleep(0.1)  # Pause for 1 second between prints
-positions0 = robot.servo.get_positions()
-for item in positions0:
-    if item[0] == 16:
-        print(item[0])
-        break
+    time.sleep(0.1)
+
+print(movement_s16)
+
+print(len(movement_s16), "positions saved")
+    # print(type(positions))
+
+input("hit Enter to start moving")
+
+for item in movement_s16:
+    robot.servo.set_position(item[0], item[1])
+    time.sleep(0.1)
+
