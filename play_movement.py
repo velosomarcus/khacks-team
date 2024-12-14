24d357428cc7a6d67433aaa1b1f8e58dfdc92346
velosomarcus <44785425+velosomarcus@users.noreply.github.com>
2024-12-14 15:39:12 -0800

    testing

--- play_movement.py
+++ play_movement.py
@@ -10,17 +10,30 @@ robot = HAL("192.168.42.1")
 
 filename = input("Enter the movement filename:")
 
-if os.path.isfile(filename):
-    print("File", filename, "not found")
-    sys.exit(0)
+filename = '/Users/mveloso/Pycharm/klabs/khacks-team/' + (filename or "movements_up_down.json")
+
+
+# if os.path.isfile(filename):
+#     print("File", filename, "not found")
+#     sys.exit(0)
+
+robot.servo.set_torque_enable([i, False] for i in range(1, 17))
+robot.servo.set_torque([i, 50] for i in range(1,17))
 
 input("hit Enter to start moving")
 with open(filename, 'r') as r:
     movements = json.loads(r.read())
 
+robot.servo.set_torque_enable([i, True] for i in range(1, 17))
+robot.servo.enable_movement()
+
 for key in movements:
-    for servo in key:
-        robot.servo.set_position(servo[0], servo[1]) #, .05)
+    print([[servo[0], servo[1]] for servo in key])
+    robot.servo.set_positions([[servo[0], servo[1]] for servo in key])
+
     time.sleep(0.01)
 
+robot.servo.disable_movement()
+
+input('Hit Enter to relax the robot')
 robot.servo.set_torque_enable([(i, False) for i in range(1, 17)])
--- record_movements.py
+++ record_movements.py
@@ -11,18 +11,19 @@ robot = HAL("192.168.42.1")
 
 # positions = robot.servo.get_positions()
 suffix = input("Enter a filename for this movement:")
-filename = 'movements' + suffix + '.json'
+filename = 'movements_' + suffix + '.json'
 
-robot.servo.disable_movement()
-robot.servo.set_position(16, 45)
+# robot.servo.disable_movement()
+robot.servo.set_position(16, 0)
 # robot.servo.disable_movement()
 movements = []
-# positions = robot.servo.get_positions()
+# positions = robot
+# .servo.get_positions()
 robot.servo.set_torque_enable([(i, False) for i in range(1, 17)])
 input("hit Enter when ready to start recording the movements")
 
 start_time = time.time()
-while time.time() - start_time < 10:
+while time.time() - start_time < 6:
     # Get current positions of all servos
     positions = robot.servo.get_positions()
     # ID, position, velocity [(1, 0, 0), (2, -50, 0), (3, 5, 20)...]
@@ -31,6 +32,7 @@ while time.time() - start_time < 10:
 
     movements.append(positions)
     time.sleep(0.05)
+
 
 with open(filename, 'w') as o:
     o.write(json.dumps(movements, indent=2))
