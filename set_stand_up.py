10f2d5fd85e529281294b4d643de0690570c76ae
velosomarcus <44785425+velosomarcus@users.noreply.github.com>
2024-12-14 17:55:05 -0800

    minor change

---
+++ set_stand_up.py
@@ -0,0 +1,29 @@
+from openlch.hal import HAL
+
+robot = HAL("192.168.42.1")
+
+robot.servo.disable_movement()
+
+# robot.servo.enable_movement()
+robot.servo.set_positions([[i, 0] for i in range(1, 16)])
+
+robot.servo.set_position(4, -40)
+robot.servo.set_position(9, 40)
+
+robot.servo.set_position(2, 15)
+robot.servo.set_position(7, -15)
+
+robot.servo.set_position(3, -10)
+robot.servo.set_position(8, 10)
+
+robot.servo.set_position(1, 15)
+robot.servo.set_position(6, -15)
+
+robot.servo.set_position(5, -15)
+robot.servo.set_position(10, 15)
+
+robot.servo.set_position(15, 45)
+robot.servo.set_position(12, -45)
+
+robot.servo.set_position(11, -45)
+robot.servo.set_position(16, 45)
