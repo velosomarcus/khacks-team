3253cedfff3bdbad0eafd96ec90ee429a88ed940
velosomarcus <44785425+velosomarcus@users.noreply.github.com>
2024-12-14 16:09:09 -0800

    passing filename as a parameter to play script

--- play_movement.py
+++ play_movement.py
@@ -8,20 +8,30 @@ from openlch.hal import HAL
 
 robot = HAL("192.168.42.1")
 
-filename = input("Enter the movement filename:")
+# filename = input("Enter the movement filename: ")
+#
+# full_file_path = os.path.join(os.getcwd(), (filename or "movements_up_down.json"))
 
-filename = '/Users/mveloso/Pycharm/klabs/khacks-team/' + (filename or "movements_up_down.json")
+if len(sys.argv) < 2:
+    print("Usage: python script.py <file-name>")
+    sys.exit(1)
 
+# Get the parameter (first argument after the script name)
+parameter = sys.argv[1]
 
-# if os.path.isfile(filename):
-#     print("File", filename, "not found")
-#     sys.exit(0)
+full_file_path = os.path.join(os.getcwd(), parameter)
+print(full_file_path)
+
+if not os.path.isfile(full_file_path):
+    print("File", full_file_path, "not found")
+    sys.exit(0)
+
 
 robot.servo.set_torque_enable([i, False] for i in range(1, 17))
 robot.servo.set_torque([i, 50] for i in range(1,17))
 
 input("hit Enter to start moving")
-with open(filename, 'r') as r:
+with open(full_file_path, 'r') as r:
     movements = json.loads(r.read())
 
 robot.servo.set_torque_enable([i, True] for i in range(1, 17))
